a4c66a5c8a38d615dc3dce7d3307eab8d95601f4
LordPINE <middelhylke@gmail.com>
2024-02-07 11:48:34 +0100

    Add left click feature to corrupted infusion

--- src/main/java/com/lordpine/apcc/witchery/CorruptedInfusion.java
+++ src/main/java/com/lordpine/apcc/witchery/CorruptedInfusion.java
@@ -2,10 +2,14 @@ package com.lordpine.apcc.witchery;
 
 import java.util.List;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
@@ -25,15 +29,28 @@ public class CorruptedInfusion extends Infusion {
     }
 
     @Override
-    @SuppressWarnings({ "unchecked" })
+    public void onLeftClickEntity(ItemStack itemstack, World world, EntityPlayer player, Entity otherEntity) {
+        if (!world.isRemote) {
+            if (otherEntity instanceof EntityLivingBase) {
+                EntityLivingBase otherLivingEntity = (EntityLivingBase) otherEntity;
+                if (this.consumeCharges(world, player, 1, true)) {
+                    otherLivingEntity.addPotionEffect(new PotionEffect(Potion.wither.id, 100, 2, true));
+                    otherLivingEntity.addPotionEffect(new PotionEffect(Potion.weakness.id, 100, 2, true));
+                    otherLivingEntity.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 100, 2, true));
+                }
+            }
+        }
+    }
+
+    @Override
     public void onUsingItemTick(ItemStack itemstack, World world, EntityPlayer player, int countdown) {
         double x = player.posX;
         double y = player.posY;
         double z = player.posZ;
         if (!world.isRemote) {
             List<EntityLiving> entitiesWithinAABB = world.getEntitiesWithinAABB(
-                EntityLiving.class,
-                AxisAlignedBB.getBoundingBox(x - 5.0D, y - 5.0D, z - 5.0D, x + 5.0D, y + 5.0D, z + 5.0D));
+                    EntityLiving.class,
+                    AxisAlignedBB.getBoundingBox(x - 5.0D, y - 5.0D, z - 5.0D, x + 5.0D, y + 5.0D, z + 5.0D));
             for (EntityLiving entity : entitiesWithinAABB) {
                 double dx = x - entity.posX;
                 double dy = y - entity.posY;
